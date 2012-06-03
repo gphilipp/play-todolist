333c66ef95e27b74e8fce68ecedf042fe4f16860
Gilles Philippart <gilles.philippart@gmail.com>
2012-06-03 21:50:45 +0200

    Tasks are now listed

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,37 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.Task;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
 
-import views.html.*;
+import static views.html.index.render;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok("Hello World");
-  }
-  
+
+    static Form<Task> taskForm = form(Task.class);
+
+    public static Result index() {
+        return redirect(routes.Application.tasks());
+    }
+
+
+    public static Result tasks() {
+        return ok(render(Task.all(), taskForm));
+    }
+
+    public static Result newTask() {
+        Form<Task> filledForm = taskForm.bindFromRequest();
+        if (filledForm.hasErrors()) {
+            return badRequest(render(Task.all(), filledForm));
+        } else {
+            Task.create(filledForm.get());
+            return redirect(routes.Application.tasks());
+        }
+    }
+
+    public static Result deleteTask(Long id) {
+        Task.delete(id);
+        return redirect(routes.Application.tasks());
+    }
 }
---
+++ app/models/Task.java
@@ -0,0 +1,38 @@
+package models;
+
+import com.avaje.ebean.Ebean;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Task extends Model {
+
+    @Id
+    public Long id;
+
+    @Required
+    public String label;
+
+    public static Finder<Long,Task> find = new Finder(
+            Long.class, Task.class
+    );
+
+
+    public static List<Task> all() {
+        return find.all();
+    }
+
+    public static void create(Task task) {
+        task.save();
+    }
+
+    public static void delete(Long id) {
+        find.ref(id).delete();
+    }
+}
